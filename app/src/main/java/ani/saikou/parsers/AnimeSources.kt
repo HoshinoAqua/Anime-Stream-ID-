331a0d92d4680e0bffe74da53a6caad8b5b8c5f9
Aqua Hoshino <134200634+HoshinoAqua@users.noreply.github.com>
2023-07-24 12:23:32 +0700

    Update AnimeSources.kt

--- app/src/main/java/ani/saikou/parsers/AnimeSources.kt
+++ app/src/main/java/ani/saikou/parsers/AnimeSources.kt
@@ -13,6 +13,10 @@ import ani.saikou.parsers.anime.HentaiStream
 import ani.saikou.parsers.anime.Marin
 import ani.saikou.parsers.anime.NineAnime
 import ani.saikou.parsers.anime.Kaido
+import ani.saikou.parsers.anime.KuramaAnime
+import ani.saikou.parsers.anime.OpLoverz
+import ani.saikou.parsers.anime.OtakuDesu
+import ani.saikou.parsers.anime.Samehadaku
 
 object AnimeSources : WatchSources() {
     override val list: List<Lazier<BaseParser>> = lazyList(
@@ -23,6 +27,10 @@ object AnimeSources : WatchSources() {
         "AnimePahe" to ::AnimePahe,
         "9anime" to ::NineAnime,
         "AnimeDao" to ::AnimeDao,
+        "KuramaAnime" to ::KuramaAnime,
+        "OpLoverz" to ::Oploverz,
+        "OtakuDesu" to ::OtakuDesu,
+        "Samehadaku" to ::Samehadaku,
     )
 }
 
